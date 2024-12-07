8a178478610e987fa0187c409f5a4f9a57e3a679
Christian Bustos Betancur <111025495+ChristianBustosBetancur@users.noreply.github.com>
2024-12-07 23:53:30 +0000

    Prueba1.1

---
+++ src/main/java/gestion/Zona.java
@@ -0,0 +1,40 @@
+package gestion;
+import ZooAnimales.Animal;
+import java.util.ArrayList;
+
+public class Zona {
+	private String nombre;
+	private static Zoologico zoo;
+	private ArrayList<Animal> animales = new ArrayList<>();
+	
+	public Zona(String nombre, Zoologico zoo) {
+		this.nombre = nombre;
+		this.zoo = zoo;
+		
+	}
+	public Zona() {}
+	
+	public void agregarAnimales(Animal animal){
+		animales.add(animal);	
+	}
+	
+	public int cantidadAnimales() {
+		return animales.size();
+	}
+	public static Zoologico getZoo() {
+		return zoo;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	
+	
+
+}
