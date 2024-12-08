6554d57d2a47b37e43bbe6696e8b48cc52b30b85
Christian Bustos Betancur <111025495+ChristianBustosBetancur@users.noreply.github.com>
2024-12-08 01:20:11 +0000

    Prueba 1.7

--- src/main/java/zooAnimales/Anfibio.java
+++ src/main/java/zooAnimales/Anfibio.java
@@ -22,7 +22,7 @@ public class Anfibio extends Animal {
 	public String movimiento(){
 		return "saltar";
 	}
-	public Anfibio crearRana(String nombre,int edad, String genero){
+	public static Anfibio crearRana(String nombre,int edad, String genero){
 		Anfibio rana = new Anfibio(nombre,edad,"selva",genero,"rojo",true);
 		ranas++;
 		return rana;
